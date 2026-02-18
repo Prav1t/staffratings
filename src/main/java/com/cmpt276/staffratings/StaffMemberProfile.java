ec88a8e4734c6a8f9ea5545a4b12917a701e3c61
Prav1t <pravit.hundal05@gmail.com>
2026-02-17 22:44:54 -0800

    Update StaffMemberProfile.java

--- src/main/java/com/cmpt276/staffratings/StaffMemberProfile.java
+++ src/main/java/com/cmpt276/staffratings/StaffMemberProfile.java
@@ -1,13 +1,8 @@
 package com.cmpt276.staffratings;
 
-/**
- * Interface to demonstrate polymorphism.
- * Different roles can implement their own display title.
- */
+
 public interface StaffMemberProfile {
 
-    /**
-     * Returns a formatted display title based on role.
-     */
+   
     String displayTitle();
 }
