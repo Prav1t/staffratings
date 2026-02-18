a0d0d07d083241015398fc1d5a6555ab4beb334e
Prav1t <pravit.hundal05@gmail.com>
2026-02-17 23:14:41 -0800

    Update StaffRatingControllerIntegrationTest.java.java

--- src/test/java/com/cmpt276/staffratings/StaffRatingControllerIntegrationTest.java.java
+++ src/test/java/com/cmpt276/staffratings/StaffRatingControllerIntegrationTest.java.java
@@ -14,47 +14,63 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class StaffRatingControllerIntegrationTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc; //to simulate requests
 
     @Test
-    void getIndexReturns200AndHasRatingsModelAttribute() throws Exception {
-        mockMvc.perform(get("/staff"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("ratings"))
+    void getIndexReturns200AndHasRatingsModelAttribute() throws Exception { //
+        
+        mockMvc.perform(get("/staff")) //sends get request
+                .andExpect(status().isOk()) //excepts 200 ok
+                .andExpect(model().attributeExists("ratings")) //
                 .andExpect(view().name("index"));
+        
     }
 
     @Test
     void getNewFormReturns200AndHasEmptyRating() throws Exception {
+        
         mockMvc.perform(get("/staff/new"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("rating"))
                 .andExpect(view().name("form"));
+        
     }
 
     @Test
     void postCreateSuccessRedirectsToStaff() throws Exception {
-        mockMvc.perform(post("/staff")
+        
+        mockMvc.perform(post("/staff") //sends post request
                         .param("name", "Integration Test")
                         .param("email", "integration@test.com")
                         .param("clarity", "7")
                         .param("niceness", "7")
                         .param("knowledgeableScore", "7")
                         .param("roleType", "TA")
+                        
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/staff"));
+        
     }
 
+
+
+    
     @Test
     void postCreateFailureReturnsFormWithErrors() throws Exception {
-        // Missing required fields (like roleType, maybe name)
+        // Missing required fields 
         mockMvc.perform(post("/staff")
-                        .param("email", "bademail")     // invalid email
-                        .param("clarity", "50")          // out of range
+                        .param("email", "bademail") //for invalid email
+                        .param("clarity", "50") //for out of range
+                        
                 )
                 .andExpect(status().isOk())
                 .andExpect(view().name("form"))
                 .andExpect(model().hasErrors());
+
+        
     }
+
+
+    
 }
