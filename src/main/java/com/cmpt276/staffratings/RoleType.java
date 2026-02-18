2b512b9235502c2cba583f93e3f435a9e344d737
Prav1t <pravit.hundal05@gmail.com>
2026-02-17 22:44:31 -0800

    Update RoleType.java

--- src/main/java/com/cmpt276/staffratings/RoleType.java
+++ src/main/java/com/cmpt276/staffratings/RoleType.java
@@ -1,9 +1,6 @@
 package com.cmpt276.staffratings;
 
-/**
- * Enum representing different staff roles.
- * This satisfies the assignment requirement for roleType.
- */
+
 public enum RoleType {
 
     TA,
