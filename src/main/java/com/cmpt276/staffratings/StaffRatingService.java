729b1867d6db199ad4ff212bdad9cd2deb6f1793
Prav1t <pravit.hundal05@gmail.com>
2026-02-17 23:08:03 -0800

    Update StaffRatingService.java

--- src/main/java/com/cmpt276/staffratings/StaffRatingService.java
+++ src/main/java/com/cmpt276/staffratings/StaffRatingService.java
@@ -3,7 +3,7 @@ package com.cmpt276.staffratings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.List; //list colection
 
 @Service
 public class StaffRatingService {
@@ -11,20 +11,30 @@ public class StaffRatingService {
     @Autowired
     private StaffRatingRepository repository;
 
-    public List<StaffRating> getAll() {
-        return repository.findAll();
+    public List<StaffRating> getAll() { //gets all ratings
+        
+        return repository.findAll(); //gets from the database
+        
     }
 
-    public StaffRating getById(Long id) {
-        return repository.findById(id).orElseThrow();
+    public StaffRating getById(Long id) { //gets it by the id
+        
+        return repository.findById(id).orElseThrow(); //to throw error
+        
     }
 
-    public StaffRating save(StaffRating rating) {
-        return repository.save(rating);
+    public StaffRating save(StaffRating rating) { //to save or update the rating
+        
+        return repository.save(rating); //stores it in the database
+        
     }
 
-    public void delete(Long id) {
-        repository.deleteById(id);
+    public void delete(Long id) { //to delete by id
+        
+        repository.deleteById(id); //removes it from the db
+        
     }
+
+    
 }
 
